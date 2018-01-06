254a47f114da5c060370c0a1067bcabd2d88f81f
albertolluch <albertolluch@gmail.com>
2018-01-06 14:57:12 +0100

    workflow example

--- tutorial/workflow-0/main.go
+++ tutorial/workflow-0/main.go
@@ -21,11 +21,11 @@ func main() {
 
 	// Tuple space for the rules regulating the order of tasks
 	rules := NewSpace("tcp://localhost:31415/rules")
-	rules.Put("<", "Alice", "Bob")
-	rules.Put("<", "Alice", "Charlie")
-	rules.Put("<", "Bob", "Dave")
-	rules.Put("<", "Charlie", "Dave")
-	rules.Put("<", "Dave", "Master")
+	rules.Put("Alice", "Bob")
+	rules.Put("Alice", "Charlie")
+	rules.Put("Bob", "Dave")
+	rules.Put("Charlie", "Dave")
+	rules.Put("Dave", "Master")
 
 	// Tuple space for the tokens used to signal termination
 	tokens := NewSpace("tcp://localhost:31146/tokens")
@@ -53,12 +53,12 @@ func coordinateTask(task func(), me string, rules *Space, tokens *Space) {
 
 	// Read order constraints
 	var who string
-	before, _ := rules.QueryAll("<", &who, me)
-	after, _ := rules.QueryAll("<", me, &who)
+	before, _ := rules.QueryAll(&who, me)
+	after, _ := rules.QueryAll(me, &who)
 
 	// Wait for tokens of previous tasks
 	for _, edge := range before {
-		who = (edge.GetFieldAt(1)).(string)
+		who = (edge.GetFieldAt(0)).(string)
 		fmt.Printf("%s is waiting for %s...\n", me, who)
 		tokens.Get("token", who, me)
 		fmt.Printf("%s got token from %s...\n", me, who)
@@ -69,7 +69,7 @@ func coordinateTask(task func(), me string, rules *Space, tokens *Space) {
 
 	// Send tokens to tasks that come next
 	for _, edge := range after {
-		who = (edge.GetFieldAt(2)).(string)
+		who = (edge.GetFieldAt(1)).(string)
 		tokens.Put("token", me, who)
 		fmt.Printf("%s sent token to %s...\n", me, who)
 	}
